f0a87b7251e3afa548e8af84684f74b47b87f59f
Renji Joseph Sabu <renjiniravath@gmail.com>
2023-04-24 21:18:51 -0400

    add shared preferences request completed

--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -4,3 +4,8 @@ export const checkIfUserIsAuthorized = (username) => {
     const url = `users/${username}/check`
     return handler("GET", url)
 }
+
+export const getAuthorizedUsers = () => {
+    const url = `users`
+    return handler("GET", url)
+}
